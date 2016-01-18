ecdec5e63cf5badd0fd9e178259ee75f4b8a660b
Julie Duverger <julie.duverger@live.fr>
2016-01-18 17:24:59 +0100

    slideInDown
    modif java

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -5,276 +5,286 @@
 
 $(document).ready(function(){
 
-    $(window).on("scroll",function(){
-        console.log($(this).scrollTop());
-    });
-        
-     /* MENU RESPONSIVE */   
+    $(window).load(function(){
 
-     $("#nav__icon").click(function(e){
-        e.preventDefault();
-        $("body").toggleClass("with--sidebar");
-     })
+        $(window).on("scroll",function(){
+            console.log($(this).scrollTop());
+        });
+            
+         /* MENU RESPONSIVE */   
 
-    /* ACCUEIL */
+         $("#nav__icon").click(function(e){
+            e.preventDefault();
+            $("body").toggleClass("with--sidebar");
+         })
 
-    $("#accueil_estampille2").hide();
-    $("#accueil_estampille2").delay(1000).hide().fadeIn(1500);
+        /* ACCUEIL */
+
+        $("#accueil_estampille2").hide();
+        $("#accueil_estampille2").delay(1000).hide().fadeIn(1500);
 
 
-    /* LOADER */
+        /* LOADER */
 
-    $(".fakeloader").fakeLoader({
-        timeToHide:1200,
-        bgColor:"#F6ECEC",
-        spinner:"spinner5"
-    });    
+        $(".fakeloader").fakeLoader({
+            timeToHide:1200,
+            bgColor:"#F6ECEC",
+            spinner:"spinner5"
+        });    
 
-    /*-------- 
-    SlideInDown
-    ---------*/
+        /*-------- 
+        SlideInDown
+        ---------*/
 
-    $("#essai").addClass("slideInDown");
-    $(".presentation_projet h3").addClass("slideInDown");
+        $(".titreh2").addClass("slideInDown");
+        $(".titreh3").addClass("slideInDown");
+        $(".titreh4").addClass("slideInDown");
+        $(".titrehp").addClass("slideInDown");
 
-    /*-------- 
-    Frise haut 
-    ---------*/
-    var $frise_haut = $("#frise_haut").drawsvg({
-            duration: 2000,
+        /*-------- 
+        fadeInDown
+        ---------*/
+
+        /*-------- 
+        Frise haut 
+        ---------*/
+        var $frise_haut = $("#frise_haut").drawsvg({
+                duration: 2000,
+            });
+
+        $(window).on("scroll",function(){
+
+            
+            if ($(window).width() < 1290) {
+                if ($(window).scrollTop() > 215 && $(window).scrollTop() < 230) {
+                    $frise_haut.drawsvg('animate');
+                } 
+            } 
+
+            else {
+                if ($(window).scrollTop() > 25 && $(window).scrollTop() < 50) {
+                    $frise_haut.drawsvg('animate');
+                } 
+            } 
+        });
+            
+
+        /*-------
+        Frise bas
+        ---------*/
+
+        var $frise_bas = $("#frise_bas").drawsvg({
+        	duration : 3000,
+        	});
+
+        $(window).on("scroll",function(){
+
+            
+            if ($(window).width() < 1290) {
+                if ($(window).scrollTop() > 1650 && $(window).scrollTop() < 1680) {
+                    $frise_bas.drawsvg('animate');
+                } 
+            } 
+
+            else if ($(window).width() > 1290 && $(window).width() < 1450){
+                 if ($(window).scrollTop() > 1800 && $(window).scrollTop() < 1830) {
+                    $frise_bas.drawsvg('animate');
+                } 
+            }
+
+            else {
+                if ($(window).scrollTop() > 2180 && $(window).scrollTop() < 2230) {
+                    $frise_bas.drawsvg('animate');
+                } 
+            } 
         });
 
-    $(window).on("scroll",function(){
+         /*-------
+        Frise bas soa
+        ---------*/
 
-        
-        if ($(window).width() < 1290) {
-            if ($(window).scrollTop() > 215 && $(window).scrollTop() < 230) {
-                $frise_haut.drawsvg('animate');
+        var $frise_bas1 = $("#frise_bas_soa1").drawsvg({
+            duration : 3000,
+            });
+
+        $(window).on("scroll",function(){
+
+            
+            if ($(window).width() < 1290) {
+                if ($(window).scrollTop() > 970 && $(window).scrollTop() < 1000) {
+                    $frise_bas1.drawsvg('animate');
+                } 
             } 
-        } 
 
-        else {
-            if ($(window).scrollTop() > 25 && $(window).scrollTop() < 50) {
-                $frise_haut.drawsvg('animate');
+            else if ($(window).width() > 1290 && $(window).width() < 1450){
+                 if ($(window).scrollTop() > 1140 && $(window).scrollTop() < 1150) {
+                    $frise_bas1.drawsvg('animate');
+                } 
             } 
-        } 
-    });
-        
 
-    /*-------
-    Frise bas
-    ---------*/
+            else if ($(window).width() > 1450 && $(window).width() < 1690){
+                 if ($(window).scrollTop() > 950 && $(window).scrollTop() < 1000) {
+                    $frise_bas1.drawsvg('animate');
+                } 
+            }
 
-    var $frise_bas = $("#frise_bas").drawsvg({
-    	duration : 3000,
-    	});
+            else if ($(window).width() > 1690 && $(window).width() < 1950){
+                 if ($(window).scrollTop() > 850 && $(window).scrollTop() < 880) {
+                    $frise_bas1.drawsvg('animate');
+                } 
+            }               
 
-    $(window).on("scroll",function(){
-
-        
-        if ($(window).width() < 1290) {
-            if ($(window).scrollTop() > 1650 && $(window).scrollTop() < 1680) {
-                $frise_bas.drawsvg('animate');
+            else {
+                if ($(window).scrollTop() > 1320 && $(window).scrollTop() < 1350) {
+                    $frise_bas1.drawsvg('animate');
+                } 
             } 
-        } 
+        });
 
-        else if ($(window).width() > 1290 && $(window).width() < 1450){
-             if ($(window).scrollTop() > 1800 && $(window).scrollTop() < 1830) {
-                $frise_bas.drawsvg('animate');
+         /*-------
+        Frise bas toietmoi
+        ---------*/
+
+            var $frise_bas2 = $("#frise_bas_toietmoi2").drawsvg({
+            duration : 3000,
+            });
+
+        $(window).on("scroll",function(){
+
+            
+            if ($(window).width() < 1290) {
+                if ($(window).scrollTop() > 970 && $(window).scrollTop() < 1000) {
+                    $frise_bas.drawsvg('animate');
+                } 
             } 
+
+            else if ($(window).width() > 1290 && $(window).width() < 1450){
+                 if ($(window).scrollTop() > 1310 && $(window).scrollTop() < 1330) {
+                    $frise_bas2.drawsvg('animate');
+                } 
+            }        
+
+            else {
+                if ($(window).scrollTop() > 1320 && $(window).scrollTop() < 1350) {
+                    $frise_bas2.drawsvg('animate');
+                } 
+            } 
+        });
+
+        /*--------------
+        Frise_verticale
+        ---------------*/
+        var $frise_verticale = $("#frise_verticale").drawsvg({
+        	duration : 2750,
+        	});
+
+        $(window).on("scroll",function(){
+
+            
+            if ($(window).width() < 1290) {
+                if ($(window).scrollTop() > 850 && $(window).scrollTop() < 880) {
+                    $frise_verticale.drawsvg('animate');
+                } 
+            } 
+
+            else {
+                if ($(window).scrollTop() > 800 && $(window).scrollTop() < 1100) {
+                    $frise_verticale.drawsvg('animate');
+                } 
+            } 
+        });
+
+        /*--------------
+        Frise_verticale1
+        ---------------*/
+
+        var $frise_verticale1 = $("#frise_verticale1").drawsvg({
+        	duration : 2750,
+        	});
+
+
+        $(window).on("scroll",function(){
+
+            
+            if ($(window).width() < 1290) {
+                if ($(window).scrollTop() > 1045 && $(window).scrollTop() < 1070) {
+                    $frise_verticale1.drawsvg('animate');
+                } 
+            } 
+
+            else {
+                if ($(window).scrollTop() > 1300 && $(window).scrollTop() < 1400) {
+                    $frise_verticale1.drawsvg('animate');
+                } 
+            } 
+        });
+
+        /*----------------------
+        Frise_verticale_mindful
+        -----------------------*/
+
+        var $frise_verticale_mindful = $("#frise_verticale_mindful").drawsvg({
+        	duration : 2750,
+        	});
+        $(window).on("scroll",function(){
+            if ($(this).scrollTop() > 800 && $(this).scrollTop() < 1100) {
+        $frise_verticale_mindful.drawsvg('animate');
         }
 
-        else {
-            if ($(window).scrollTop() > 2180 && $(window).scrollTop() < 2230) {
-                $frise_bas.drawsvg('animate');
-            } 
-        } 
-    });
-
-     /*-------
-    Frise bas soa
-    ---------*/
-
-    var $frise_bas1 = $("#frise_bas_soa1").drawsvg({
-        duration : 3000,
         });
 
-    $(window).on("scroll",function(){
+        /*--------------
+        Frise_soustitre
+        ----------------*/
 
-        
-        if ($(window).width() < 1290) {
-            if ($(window).scrollTop() > 970 && $(window).scrollTop() < 1000) {
-                $frise_bas1.drawsvg('animate');
-            } 
-        } 
+        var $svg = $("#frise_soustitre").drawsvg({
+        	duration : 5000,
+        	stagger: 500,
+        	reverse: false,
+        	});
 
-        else if ($(window).width() > 1290 && $(window).width() < 1450){
-             if ($(window).scrollTop() > 1140 && $(window).scrollTop() < 1150) {
-                $frise_bas1.drawsvg('animate');
-            } 
-        } 
+        $svg.drawsvg('animate');
 
-        else if ($(window).width() > 1450 && $(window).width() < 1690){
-             if ($(window).scrollTop() > 950 && $(window).scrollTop() < 1000) {
-                $frise_bas1.drawsvg('animate');
-            } 
-        }
+        /* ----------------  */
 
-        else if ($(window).width() > 1690 && $(window).width() < 1950){
-             if ($(window).scrollTop() > 850 && $(window).scrollTop() < 880) {
-                $frise_bas1.drawsvg('animate');
-            } 
-        }               
+        /* Page Transition  */
 
-        else {
-            if ($(window).scrollTop() > 1320 && $(window).scrollTop() < 1350) {
-                $frise_bas1.drawsvg('animate');
-            } 
-        } 
-    });
 
-     /*-------
-    Frise bas toietmoi
-    ---------*/
+          $(".animsition").animsition({
+            inClass: 'fade-in',
+            outClass: 'fade-out',
+            inDuration: 1000,
+            outDuration: 1200,
+            linkElement: '.animsition-link',
+            // e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
+            loading: true,
+            loadingParentElement: 'body', //animsition wrapper element
+            loadingClass: 'animsition-loading',
+            loadingInner: '', // e.g '<img src="loading.svg" />'
+            timeout: false,
+            timeoutCountdown: 5000,
+            onLoadEvent: true,
+            browser: [ 'animation-duration', '-webkit-animation-duration'],
+            // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+            // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+            overlay : false,
+            overlayClass : 'animsition-overlay-slide',
+            overlayParentElement : 'body',
+            transition: function(url){ window.location.href = url; }
+          });
 
-        var $frise_bas2 = $("#frise_bas_toietmoi2").drawsvg({
-        duration : 3000,
+
+        /*---------
+        NAVIGATION
+        -----------*/
+
+        $(".navprojetgauche").hover(function() {
+            $(this).toggleClass("nav_class1");
         });
 
-    $(window).on("scroll",function(){
+        $(".navprojetdroit").hover(function() {
+            $(this).toggleClass("nav_class1");
+        });
 
-        
-        if ($(window).width() < 1290) {
-            if ($(window).scrollTop() > 970 && $(window).scrollTop() < 1000) {
-                $frise_bas.drawsvg('animate');
-            } 
-        } 
-
-        else if ($(window).width() > 1290 && $(window).width() < 1450){
-             if ($(window).scrollTop() > 1310 && $(window).scrollTop() < 1330) {
-                $frise_bas2.drawsvg('animate');
-            } 
-        }        
-
-        else {
-            if ($(window).scrollTop() > 1320 && $(window).scrollTop() < 1350) {
-                $frise_bas2.drawsvg('animate');
-            } 
-        } 
-    });
-
-    /*--------------
-    Frise_verticale
-    ---------------*/
-    var $frise_verticale = $("#frise_verticale").drawsvg({
-    	duration : 2750,
-    	});
-
-    $(window).on("scroll",function(){
-
-        
-        if ($(window).width() < 1290) {
-            if ($(window).scrollTop() > 850 && $(window).scrollTop() < 880) {
-                $frise_verticale.drawsvg('animate');
-            } 
-        } 
-
-        else {
-            if ($(window).scrollTop() > 800 && $(window).scrollTop() < 1100) {
-                $frise_verticale.drawsvg('animate');
-            } 
-        } 
-    });
-
-    /*--------------
-    Frise_verticale1
-    ---------------*/
-
-    var $frise_verticale1 = $("#frise_verticale1").drawsvg({
-    	duration : 2750,
-    	});
-
-
-    $(window).on("scroll",function(){
-
-        
-        if ($(window).width() < 1290) {
-            if ($(window).scrollTop() > 1045 && $(window).scrollTop() < 1070) {
-                $frise_verticale1.drawsvg('animate');
-            } 
-        } 
-
-        else {
-            if ($(window).scrollTop() > 1300 && $(window).scrollTop() < 1400) {
-                $frise_verticale1.drawsvg('animate');
-            } 
-        } 
-    });
-
-    /*----------------------
-    Frise_verticale_mindful
-    -----------------------*/
-
-    var $frise_verticale_mindful = $("#frise_verticale_mindful").drawsvg({
-    	duration : 2750,
-    	});
-    $(window).on("scroll",function(){
-        if ($(this).scrollTop() > 800 && $(this).scrollTop() < 1100) {
-    $frise_verticale_mindful.drawsvg('animate');
-    }
-
-    });
-
-    /*--------------
-    Frise_soustitre
-    ----------------*/
-
-    var $svg = $("#frise_soustitre").drawsvg({
-    	duration : 5000,
-    	stagger: 500,
-    	reverse: false,
-    	});
-
-    $svg.drawsvg('animate');
-
-    /* ----------------  */
-
-    /* Page Transition  */
-
-
-      $(".animsition").animsition({
-        inClass: 'fade-in',
-        outClass: 'fade-out',
-        inDuration: 1000,
-        outDuration: 1200,
-        linkElement: '.animsition-link',
-        // e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
-        loading: true,
-        loadingParentElement: 'body', //animsition wrapper element
-        loadingClass: 'animsition-loading',
-        loadingInner: '', // e.g '<img src="loading.svg" />'
-        timeout: false,
-        timeoutCountdown: 5000,
-        onLoadEvent: true,
-        browser: [ 'animation-duration', '-webkit-animation-duration'],
-        // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-        // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-        overlay : false,
-        overlayClass : 'animsition-overlay-slide',
-        overlayParentElement : 'body',
-        transition: function(url){ window.location.href = url; }
-      });
-
-
-    /*---------
-    NAVIGATION
-    -----------*/
-
-    $(".navprojetgauche").hover(function() {
-        $(this).toggleClass("nav_class1");
-    });
-
-    $(".navprojetdroit").hover(function() {
-        $(this).toggleClass("nav_class1");
     });
 });
