6bdf93feaa330dcda5ea63d51a340ab495e9f1be
Julie Duverger <julie.duverger@live.fr>
2016-03-08 16:49:59 +0100

    Motifs transition et lien index

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -258,6 +258,7 @@ $(document).ready(function(){
             inClass: 'fade-in',
             outClass: 'fade-out',
             inDuration: 1000,
+            outDelay: 2000,
             outDuration: 1200,
             linkElement: '.animsition-link',
             // e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
@@ -277,6 +278,28 @@ $(document).ready(function(){
             transition: function(url){ window.location.href = url; }
           });
 
+            $(".animsition-overlay").animsition({
+                inClass: 'overlay-slide-in-left',
+                outClass: 'overlay-slide-out-left',
+                inDuration: 1500,
+                outDuration: 800,
+                linkElement: '.animsition-link',
+                // e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
+                loading: true,
+                loadingParentElement: 'body', //animsition wrapper element
+                loadingClass: 'animsition-loading',
+                loadingInner: '', // e.g '<img src="loading.svg" />'
+                timeout: false,
+                timeoutCountdown: 5000,
+                onLoadEvent: true,
+                browser: [ 'animation-duration', '-webkit-animation-duration'],
+                // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+                // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+                overlay : true,
+                overlayClass : 'animsition-overlay-slide',
+                overlayParentElement : 'body',
+                transition: function(url){ window.location.href = url; }
+            });
 
         /*---------
         NAVIGATION
